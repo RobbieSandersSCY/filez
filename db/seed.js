dbbb79122621e56a69887a30f3176812fa8aef6d
Robbie Sanders <sande290@gmail.com>
2026-02-23 16:40:06 -0500

    add functions to seed file

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,5 @@
 import db from "#db/client";
+import { createFolder, createFile } from "./queries/folders.js";
 
 await db.connect();
 await seed();
@@ -6,5 +7,28 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  // TODO
+  await seedFolder();
+  await seedFile();
+}
+
+async function seedFolder() {
+  for (let folderCount = 1; folderCount <= 3; folderCount++) {
+    const folder = {
+      name: "folder_" + folderCount,
+    };
+    await createFolder(folder);
+  }
+}
+
+async function seedFile() {
+  for (let folder = 1; folder <= 3; folder++) {
+    for (let fileCount = 1; fileCount <= 5; fileCount++) {
+      const file = {
+        name: "file_" + fileCount,
+        size: Math.floor(Math.random() * 1000),
+        folder_id: folder,
+      };
+      await createFile(file);
+    }
+  }
 }
