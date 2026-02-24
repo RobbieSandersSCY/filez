96d6f6611760c7c76c5ef0abcbdcba7788c5c0c9
Robbie Sanders <sande290@gmail.com>
2026-02-23 20:19:03 -0500

    refactor files and folders

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 import express from "express";
 const app = express();
 export default app;
+
+import folderRouter from "./api/folders.js";
+import fileRouter from "./api/files.js";
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Filez API");
+});
+
+app.use("/files", fileRouter);
+
+app.use("/folders", folderRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Sorry! Something went wrong :(");
+});
